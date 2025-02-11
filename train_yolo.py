7bb8c75a38756186f93fad5e6f9d8dfcf33025e5
Jonathan <mckibbin16@gmail.com>
2025-02-11 13:14:12 +0000

    updated train_yolo to use GPU and batch size 16

--- train_yolo.py
+++ train_yolo.py
@@ -6,10 +6,10 @@ model = YOLO("yolov8n.pt")
 # Train the model
 model.train(
     data="dataset.yaml",  # Path to dataset.yaml
-    epochs=5,                      # Number of training epochs
+    epochs=50,                      # Number of training epochs
     imgsz=640,                      # Image size
-    batch=2,                        # Batch size
-    device="cpu"                    # Use GPU
+    batch=16,                        # Batch size
+    device="cuda"                    # Use GPU
 )
 
 # Save the trained model
