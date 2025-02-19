30a29d6986c6c842c66f5996a25f43d24693b4ee
Oisin Toner <oisinst@talktalk.net>
2025-02-19 15:20:35 +0000

    Updated Dataset. Dropped Tattoo

--- predict.py
+++ predict.py
@@ -1,10 +1,11 @@
 from ultralytics import YOLO
 
 #Route to best.pt required here
-model = YOLO("runs/best.pt")
+# model = YOLO("runs/detect/train29/weights/best.pt")
+model = YOLO("runs/detect/train36/weights/best.pt")
 
 #This can point to a specific image or a directory
-results = model.predict('example/dir/of/imgs', conf=0.25)
+results = model.predict('test/images', conf=0.05)
 
 #run through the results of the model predictions
 for result in results:
--- train_yolo.py
+++ train_yolo.py
@@ -2,13 +2,15 @@ from ultralytics import YOLO
 
 
 model = YOLO("yolov8n.pt")
+# model = YOLO("best.pt")
 
 # Train the model
 model.train(
     data="dataset.yaml",  # Path to dataset.yaml
-    epochs=50,                      # Number of training epochs
+    epochs=500,                      # Number of training epochs
     imgsz=640,                      # Image size
-    batch=16,                        # Batch size
+    batch=10,                        # Batch size
+    save=True,
     device="cuda"                    # Use GPU
 )
 
